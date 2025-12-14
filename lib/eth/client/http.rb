24046a5d51d412115e140b38d24bcc479aa2118e
Yuri Smirnov <tycoooon@gmail.com>
2025-12-14 07:57:40 +0300

    wip

--- lib/eth/client/http.rb
+++ lib/eth/client/http.rb
@@ -20,7 +20,10 @@ module Eth
 
   # Provides an HTTP/S-RPC client with basic authentication.
   class Client::Http < Client
-    CLIENT = HTTPX.plugin(:persistent).with(headers: { "Content-Type" => "application/json" })
+    CLIENT = HTTPX
+      .plugin(:persistent)
+      .with(headers: { "Content-Type" => "application/json" })
+      .with(timeout: Eth.client_timeout)
 
     # The host of the HTTP endpoint.
     attr_reader :host
@@ -67,7 +70,7 @@ module Eth
     # @return [String] a JSON-encoded response.
     def send_request(payload)
       debug_http { "send_request #{@uri} #{payload}" }
-      response = CLIENT.post(@uri, body: payload)
+      response = CLIENT.post(@uri, body: payload).raise_for_status
       debug_http { "#{response.inspect}" }
       debug_http { "#{response.body}" }
       response.body.to_s
