f572f78eda7974cf4b1b7d64ca2918a82cfa21a4
Yuri Smirnov <tycoooon@gmail.com>
2026-03-03 17:52:55 +0300

    wip

--- lib/eth/client/http.rb
+++ lib/eth/client/http.rb
@@ -28,7 +28,7 @@ module Eth
           .with(headers: { "Content-Type" => "application/json" })
           .with(timeout: Eth.client_timeout)
           .with(resolver_class: Eth.resolver_class)
-          .with(resolver_options: { timeout: Eth.resolver_timeout })
+          .with(resolver_options: { timeouts: Eth.resolver_timeout })
     end
 
     # The host of the HTTP endpoint.
